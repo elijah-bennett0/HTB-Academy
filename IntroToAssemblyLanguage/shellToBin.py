2c6aa225fbd446fb0d57be7e68e0a016e07c663a
elijah-bennett0 <elijahb451@gmail.com>
2024-09-09 16:32:00 +0200

    Shellcode section files

---
+++ IntroToAssemblyLanguage/shellToBin.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Convert shellcode to a binary
+
+import sys, os, stat
+from pwn import *
+
+context(os="linux", arch="amd64", log_level="error")
+
+ELF.from_bytes(unhex(sys.argv[1])).save(sys.argv[2])
+os.chmod(sys.argv[2], stat.S_IEXEC)
