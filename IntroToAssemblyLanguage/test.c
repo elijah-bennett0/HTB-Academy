6deabc3979c2d7ad3ae04fecefec29ac5b8411c0
ginsu <elijahb451@gmail.com>
2024-09-15 08:14:39 -0400

    Still trynna figure this shit out

--- IntroToAssemblyLanguage/test.c
+++ IntroToAssemblyLanguage/test.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 int main() {
-	char *args[] = {"/usr/bin/cat", "flag.txt", (char*) NULL};
+	char *args[] = {"/bin/cat", "flag.txt", (char*) NULL};
 	execve(args[0], args, NULL);
 	return 0;
 };
