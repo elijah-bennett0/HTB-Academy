856c17a5ac6e6330220d2091571001046eaf0370
elijah-bennett0 <elijahb451@gmail.com>
2024-09-27 14:35:41 +0200

    nullFind.py bug fix and task2 finish

--- IntroToAssemblyLanguage/SkillsAssessment/task2/flag.s
+++ IntroToAssemblyLanguage/SkillsAssessment/task2/flag.s
@@ -3,18 +3,19 @@ global _start
 section .text
 _start:
     ; push './flg.txt\x00'
-    xor al, al
-    push al        ; push NULL string terminator
-    mov rdi, '/flg.txt' ; rest of file name
+    xor sil, sil
+    push si        ; push NULL string terminator
+    mov rdi, 'flg.txt' ; rest of file name
     push rdi            ; push to stack
     ; open('rsp', 'O_RDONLY')
-    mov ah, 2          ; open syscall number
+    mov al, 2          ; open syscall number
     mov rdi, rsp        ; move pointer to filename          ; set O_RDONLY flag
     syscall
 
     ; read file
     lea rsi, [rdi]      ; pointer to opened file
-    mov rdi, al        ; set fd to rax from open syscall         ; read syscall number
+    mov rdi, rax        ; set fd to rax from open syscall         ; read syscall number
+    xor al, al
     mov dl, 24         ; size to read
     syscall
 
--- IntroToAssemblyLanguage/nullFind.py
+++ IntroToAssemblyLanguage/nullFind.py
@@ -1,4 +1,4 @@
 import sys
 
 shellcode = sys.argv[1]
-print("%d bytes - Found NULL byte" % len(shellcode)) if [i for i in shellcode if i == 0] else print("%d bytes - No NULL bytes" % len(shellcode))
+print("%d bytes - Found NULL byte" % (len(shellcode)/2)) if [i for i in shellcode if i == 0] else print("%d bytes - No NULL bytes" % (len(shellcode)/2))
